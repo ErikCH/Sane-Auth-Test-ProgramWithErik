5664e95c682a4a8548a9a53da2856562e9634435
Erik Hanchett <admin@programwitherik.com>
2015-08-18 22:40:07 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a simple authentication example use SANE and sane-auth. 
+For more information visit [www.programwitherik.com](http://www.programwitherik.com)
+
---
+++ client/app/router.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.resource('user', function() {});
+
+  this.route("login");
+  this.route("register");
+  this.route('protected');
+});
+
+
+
+export default Router;
